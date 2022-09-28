ab98a651f5bf386d1e27c611de48cfb2fe675a58
Sam Avis <avissam96@gmail.com>
2022-09-28 18:14:54 +0100

    Basic bitss example

---
+++ example/example.cpp
@@ -0,0 +1,11 @@
+#include "ellib.h"
+
+int main(int argc, char** argv) {
+  ellib::mpiInit(&argc, &argv);
+        
+  ellib::Lj3d pot;
+  ellib::State s1 = pot.newState({0,0});
+  ellib::State s2 = pot.newState({0,0});
+  ellib::Bitss bitss(s1, s2);
+  return 0;
+}
