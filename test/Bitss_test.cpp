b3b4bc4c32c73e41881472b15126619ba7d7a914
Sam Avis <avissam96@gmail.com>
2022-10-05 15:04:47 +0100

    TEST: Construct Bitss using a minimiser object

--- test/Bitss_test.cpp
+++ test/Bitss_test.cpp
@@ -1,6 +1,5 @@
 #include "Bitss.h"
 
-#include <vector>
 #include "minim/Lj3d.h"
 #include "minim/Lbfgs.h"
 #include "minim/Fire.h"
@@ -16,9 +15,11 @@ TEST(BitssTest, DefaultInitialisation) {
   State s1 = pot.newState({0,0});
   State s2 = pot.newState({1,0});
   Bitss bitss(s1, s2);
+
   // Check minimiser is Lbfgs
   EXPECT_NO_THROW(dynamic_cast<Lbfgs&>(*bitss.minimiser));
   EXPECT_THROW(dynamic_cast<Fire&>(*bitss.minimiser), std::bad_cast);
+
   // State coordinates
   auto coords = bitss.state.getCoords();
   auto coords1 = std::vector<double>(coords.begin(), coords.begin()+s1.ndof);
@@ -26,11 +27,25 @@ TEST(BitssTest, DefaultInitialisation) {
   EXPECT_EQ(bitss.state.ndof, s1.ndof+s2.ndof);
   EXPECT_EQ(coords1, s1.getCoords());
   EXPECT_EQ(coords2, s2.getCoords());
+
   // Check single state potentials are correct (will be possible if Args is removed and state.pot made public)
   // ASSERT_NO_THROW(dynamic_cast<Bitss::BitssPotential&>(*bitss.state._pot));
   // auto bitssPot = static_cast<Bitss::BitssPotential&>(*bitss.state._pot);
   // EXPECT_NO_THROW(dynamic_cast<Lj3d&>(*bitssPot.state1._pot));
   // EXPECT_NO_THROW(dynamic_cast<Lj3d&>(*bitssPot.state2._pot));
+}
+
+
+TEST(BitssTest, MinimiserConstructor) {
+  Lj3d pot;
+  State s1 = pot.newState({0,0});
+  State s2 = pot.newState({1,0});
+
+  // Explicitly pass minimiser object
+  auto min = std::unique_ptr<Fire>(new Fire);
+  Bitss bitss(s1, s2, std::move(min));
+  EXPECT_NO_THROW(dynamic_cast<Fire&>(*bitss.minimiser));
+  EXPECT_THROW(dynamic_cast<Lbfgs&>(*bitss.minimiser), std::bad_cast);
 }
 
 
