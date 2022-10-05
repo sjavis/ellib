5d2df74a02198619f8d12b74f9c7b4e97adefce4
Sam Avis <avissam96@gmail.com>
2022-10-05 14:48:58 +0100

    TEST: Bitss initialisation

--- test/Bitss_test.cpp
+++ test/Bitss_test.cpp
@@ -1,29 +1,47 @@
 #include "Bitss.h"
+
+#include <vector>
 #include "minim/Lj3d.h"
-#include "minim/State.h"
 #include "minim/Lbfgs.h"
+#include "minim/Fire.h"
 #include "minim/utils/mpi.h"
 
 #include "gtest/gtest.h"
 
+using namespace ellib;
 
-TEST(BitssTest, Test1) {
-  ellib::Lj3d pot;
-  ellib::State s1 = pot.newState({0,0});
-  ellib::State s2 = pot.newState({0,0});
-  ellib::Bitss bitss(s1, s2);
-  EXPECT_EQ(1, 1);
+
+TEST(BitssTest, DefaultInitialisation) {
+  Lj3d pot;
+  State s1 = pot.newState({0,0});
+  State s2 = pot.newState({1,0});
+  Bitss bitss(s1, s2);
+  // Check minimiser is Lbfgs
+  EXPECT_NO_THROW(dynamic_cast<Lbfgs&>(*bitss.minimiser));
+  EXPECT_THROW(dynamic_cast<Fire&>(*bitss.minimiser), std::bad_cast);
+  // State coordinates
+  auto coords = bitss.state.getCoords();
+  auto coords1 = std::vector<double>(coords.begin(), coords.begin()+s1.ndof);
+  auto coords2 = std::vector<double>(coords.begin()+s1.ndof, coords.end());
+  EXPECT_EQ(bitss.state.ndof, s1.ndof+s2.ndof);
+  EXPECT_EQ(coords1, s1.getCoords());
+  EXPECT_EQ(coords2, s2.getCoords());
+  // Check single state potentials are correct (will be possible if Args is removed and state.pot made public)
+  // ASSERT_NO_THROW(dynamic_cast<Bitss::BitssPotential&>(*bitss.state._pot));
+  // auto bitssPot = static_cast<Bitss::BitssPotential&>(*bitss.state._pot);
+  // EXPECT_NO_THROW(dynamic_cast<Lj3d&>(*bitssPot.state1._pot));
+  // EXPECT_NO_THROW(dynamic_cast<Lj3d&>(*bitssPot.state2._pot));
 }
 
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  ellib::mpiInit(&argc, &argv);
+  mpiInit(&argc, &argv);
 
   // Ensure only one processor prints
   ::testing::TestEventListeners& listeners =
       ::testing::UnitTest::GetInstance()->listeners();
-  if (ellib::mpi.rank != 0) {
+  if (mpi.rank != 0) {
       delete listeners.Release(listeners.default_result_printer());
   }
 
